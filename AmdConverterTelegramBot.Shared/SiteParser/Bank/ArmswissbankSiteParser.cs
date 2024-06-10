e0fb5ae8191d43a7577874baedead4d47bff83a4
warrior_spb <ivanovandrew2004@gmail.com>
2024-06-10 12:16:23 +0400

    Armswissbank data is missing #23

--- AmdConverterTelegramBot.Shared/SiteParser/Bank/ArmswissbankSiteParser.cs
+++ AmdConverterTelegramBot.Shared/SiteParser/Bank/ArmswissbankSiteParser.cs
@@ -4,7 +4,7 @@ namespace AmdConverterTelegramBot.Shared.SiteParser.Bank;
 
 public class ArmswissbankSiteParser : JsonApiRateParser
 {
-    internal override string Url { get; }
+    internal override string Url => "https://www.armswissbank.am/include/ajax.php?asd";
     protected override string ExchangeName => "Armswissbank";
     public ArmswissbankSiteParser(CurrencyParser currencyParser, CultureInfo cultureInfo) : base(currencyParser, cultureInfo)
     {
